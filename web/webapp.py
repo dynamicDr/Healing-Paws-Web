8a004ea1e5d1828857072411500b41937d7d22aa
unknown <1102310490@qq.com>
2020-03-31 14:26:50 +0800

    test PR

--- web/webapp.py
+++ web/webapp.py
@@ -2,3 +2,10 @@ from appdir import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/")
+def index():
+    return "<h1 style='color:red'>Hello World</h1>"
+
+if __name__ == "__main__":
+    app.run()
