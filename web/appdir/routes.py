d2f3134562feedc0a8713927ad45e167f5c02c9d
JackZhuu <il>
2020-04-02 22:22:29 +0800

    test

--- web/appdir/routes.py
+++ web/appdir/routes.py
@@ -2,9 +2,9 @@ from appdir import app
 from flask import render_template, flash, redirect, url_for, session, request, jsonify
 from appdir.config import Config
 
-@app.route('/')
-def login():
-    return 'hello world'
+@app.route("/")
+def index():
+    return "<h1>Hello World</h1>"
 
 
 
--- web/webapp.py
+++ web/webapp.py
@@ -3,9 +3,3 @@ from appdir import app
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route("/")
-def index():
-    return "<h1 style='color:red'>Hello World</h1>"
-
-if __name__ == "__main__":
-    app.run()
