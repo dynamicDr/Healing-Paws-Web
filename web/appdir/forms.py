9235a13f361efd0c1b22d8af972645aebfc8922c
JackZhuu <il>
2020-04-03 23:16:20 +0800

    注册写到一半了，得讨论一下用户和雇员账号注册的细节

--- web/appdir/forms.py
+++ web/appdir/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, RadioField, FileField, DateTimeField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, RadioField, FileField, DateTimeField, TextAreaField, RadioField
+from wtforms.validators import DataRequired, Email
 from flask_wtf.file import FileRequired, FileAllowed
 
 class LoginForm(FlaskForm):
@@ -11,11 +11,14 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()]) # 这里加了一个Email检测器，可以判断是否符合email格式，就不用在前端写js代码检测了
 	phone = StringField('Phone', validators=[DataRequired()])
 	dob = DateField('Date of Birth (format: YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
 	address = StringField('Address', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	password2 = PasswordField('Repeat Password', validators=[DataRequired()])
+	account_type = RadioField('Customer or Employee', choices = [('C','Customer'),('E','Employee')], validators=[DataRequired()])
+	# 暂时没必要，后面可修改得更合理，比如只有用户注册时会显示这个。
 	accept_rules = BooleanField('I accept the site rules', validators=[DataRequired()])
+	
 	submit = SubmitField('Register')
