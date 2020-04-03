5d1a2f5686c4f5189cd2684608c833bf70882ebd
JackZhuu <il>
2020-04-03 14:17:25 +0800

    database is available, next step is register

--- web/appdir/config.py
+++ web/appdir/config.py
@@ -2,4 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@47.98.48.168/test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
