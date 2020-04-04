d811effa52aa632fbb3937a75d73668af398c640
username <yu.zhang2@ucdconnect.ie>
2020-04-04 14:28:24 +0800

    review and add questions

--- web/appdir/models.py
+++ web/appdir/models.py
@@ -3,20 +3,42 @@ from appdir import db
 
 
 class Customer(db.Model):
-	__tablename__ = 'customers'
+    __tablename__ = 'customers'
 
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(120), index=True)
-	dob = db.Column(db.DateTime, index=True)
-	password_hash = db.Column(db.String(128))
-	address = db.Column(db.String(128))
-	phone = db.Column(db.String(15))
-	# posts = relationship('Post', backref='author', lazy='dynamic')
-	# topic = relationship('Topic', backref='maker', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True)
+    dob = db.Column(db.DateTime, index=True)
+    password_hash = db.Column(db.String(128))
+    address = db.Column(db.String(128))
+    phone = db.Column(db.String(15))
+   
+    # posts = relationship('Post', backref='author', lazy='dynamic')
+    # topic = relationship('Topic', backref='maker', lazy='dynamic')
 
 class User(db.Model):
-	__tablename__ = 'user'
+    __tablename__ = 'user'
 
-	id = db.Column(db.String(20), primary_key=True)
-	name = db.Column(db.String(20))
+    id = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(20))
+    questions = db.relationship('Question', backref='author', lazy='dynamic')
+    answers = db.relationship('Answer', backref='author',lazy='dynamic')
+    
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    anonymity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    answers = db.relationship('Answer', backref='question', lazy='dynamic')
+    
+    
+    def __repr__(self):
+        return '<Question asked on {} by {}: {} >'.format(str(self.timestamp)[0:10],self.author.username,self.body)
+        
+class Answer(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.String(140))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- web/appdir/routes.py
+++ web/appdir/routes.py
@@ -1,8 +1,8 @@
 from appdir import app, db
 from flask import render_template, flash, redirect, url_for, session, request, jsonify
 from appdir.config import Config
-from appdir.forms import LoginForm, RegisterForm
-from appdir.models import Customer, User
+from appdir.forms import LoginForm, RegisterForm, ReviewForm, QuestionForm
+from appdir.models import Customer, User, Question, Answer
 from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route("/")
@@ -40,4 +40,28 @@ def reset():
     db.create_all()
     return '重建所有表'
 
+@app.route('/reviewquestions')
+def reviewquestions():
+    form = ReviewForm()
+    if form.validate_on_submit():
+        prev_questions = Question.query.filter(Question.title.like('%'+form.keyword.data+'%')).all()
+        return render_template('reviewquestions.html',title="Questions Review",prev_questions=prev_questions,form=form)
+    else:
+        prev_questions = Question.query.filter()
+    return render_template('reviewquestions.html',title="Questions Review",prev_questions=prev_questions,form=form)
 
+@app.route('/addquestion')
+def addquestion():
+    form = QuestionForm()
+    if not session.get("USERNAME") is None:
+        if form.validate_on_submit():
+            username = session.get("USERNAME")
+            user_in_db = User.query.filter(User.username == username).first()
+            
+            question_db = Question(title = form.title.data, body = form.body.data, anonymity = form.anonymity.data, user=user_in_db)
+            db.session.add(question_db) 
+            db.session.commit()
+            return redirect(url_for('reviewquestions'))
+    else:
+        flash("User needs to either login or signup first")
+        return redirect(url_for('login'))
