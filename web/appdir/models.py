06deb626eafdeeba62b931adb2b708bccdf93c5d
JackZhuu <1092950198@qq.com>
2020-04-05 11:47:43 +0800

    register

--- web/appdir/models.py
+++ web/appdir/models.py
@@ -2,35 +2,30 @@ from datetime import datetime
 from appdir import db
 
 
-class Customer(db.Model):
-    __tablename__ = 'customers'
+class User(db.Model):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True)
-    dob = db.Column(db.DateTime, index=True)
     password_hash = db.Column(db.String(128))
-    address = db.Column(db.String(128))
-    phone = db.Column(db.String(15))
-   
-    # posts = relationship('Post', backref='author', lazy='dynamic')
-    # topic = relationship('Topic', backref='maker', lazy='dynamic')
-
-class User(db.Model):
-    __tablename__ = 'user'
-
-    id = db.Column(db.String(20), primary_key=True)
-    name = db.Column(db.String(20))
+    email = db.Column(db.String(120), index=True, nullable=True)
+    dob = db.Column(db.DateTime, index=True, nullable=True)
+    address = db.Column(db.String(128), nullable=True)
+    phone = db.Column(db.String(15), nullable=True)
+    is_customer = db.Column(db.Boolean, index=True)
     questions = db.relationship('Question', backref='author', lazy='dynamic')
     answers = db.relationship('Answer', backref='author',lazy='dynamic')
     
+    
 class Question(db.Model):
+    __tablename__ = 'questions'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     anonymity = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     answers = db.relationship('Answer', backref='question', lazy='dynamic')
     
     
@@ -38,7 +33,9 @@ class Question(db.Model):
         return '<Question asked on {} by {}: {} >'.format(str(self.timestamp)[0:10],self.author.username,self.body)
         
 class Answer(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    __tablename__ = 'answers'
+
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
