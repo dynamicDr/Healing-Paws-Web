952375871f4961491b8a19385dd77b9c1e387399
JackZhuu <1092950198@qq.com>
2020-04-07 21:31:17 +0800

    1

--- web/appdir/forms.py
+++ web/appdir/forms.py
@@ -9,7 +9,7 @@ class LoginForm(FlaskForm):
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
 
-class RegisterForm(FlaskForm):
+class RegisterForm_C(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	email = StringField('Email', validators=[DataRequired(), Email()]) # 这里加了一个Email检测器，可以判断是否符合email格式，就不用在前端写js代码检测了
 	phone = StringField('Phone', validators=[DataRequired()])
@@ -17,7 +17,19 @@ class RegisterForm(FlaskForm):
 	address = StringField('Address', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	password2 = PasswordField('Repeat Password', validators=[DataRequired()])
-	account_type = RadioField('Customer or Employee', choices = [('C','Customer'),('E','Employee')], validators=[DataRequired()])
+	accept_rules = BooleanField('I accept the site rules', validators=[DataRequired()])
+	submit = SubmitField('Register')
+
+class RegisterForm_E(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Repeat Password', validators=[DataRequired()])
+	loc = SelectField(
+        validators=[DataRequired('请选择注册医院所在地')],
+        choices=[(1, '北京'), (2, '上海'), (3, '成都')],
+        coerce=int
+    )
+	intro = StringField('Introduction')
 	# 暂时没必要，后面可修改得更合理，比如只有用户注册时会显示这个。
 	accept_rules = BooleanField('I accept the site rules', validators=[DataRequired()])
 	submit = SubmitField('Register')
@@ -25,6 +37,7 @@ class RegisterForm(FlaskForm):
 class AppointmentForm(FlaskForm):
 	appointment_type = RadioField('Standard or Emergency', choices = [('S','Standard'),('E','Emergency')], validators=[DataRequired()])
 	pet = SelectField(label='为哪只宠物预约', validators=[DataRequired('请选择宠物')], coerce=int)
+	location = RadioField('Which hospital', choices = [(1,'Customer'),('E','Employee')], validators=[DataRequired()])
 	submit = SubmitField('Comfirm and Submit')
 
 class ReviewForm(FlaskForm):
