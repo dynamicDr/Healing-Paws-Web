252c8c54e767b29c8fb961301767789b4e50d920
unknown <1102310490@qq.com>
2020-05-21 15:46:59 +0800

    reviewquestion

--- web/appdir/__init__.py
+++ web/appdir/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from appdir.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 mail = Mail()
 mail.init_app(app)
-
+bootstrap = Bootstrap(app)
 from appdir import routes, models
 
